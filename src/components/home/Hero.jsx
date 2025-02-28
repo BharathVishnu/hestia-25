b8907b33a82515ed7c10b5c67988b2b746656fda
vagabond-0 <220229@tkmce.ac.in>
2025-02-28 21:21:30 +0530

    Navbar Animation done

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -3,30 +3,7 @@ import '../../styles/NewHero.css'
 
 const Hero = () => {
   return (
-    <div className='section overflow-hidden' style={{ fontFamily: 'okami', position: 'relative' }}>
-      <div 
-        className='w-screen absolute'
-        style={{ 
-          height: '947px',
-          left: '0px',
-          top: '-500px',
-          backgroundImage: 'url(/src/assets/images/hero/background.webp)',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      >
-         <div 
-        className='w-screen absolute'
-        style={{ 
-          height: '947px',
-          top: '1091px',
-          top: '1091px',
-          backgroundImage: 'url(/src/assets/images/hero/background.webp)',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      ></div>
-      </div>
+    <div className='section' style={{ fontFamily: 'okami' }}>
       <div className="text-center absolute inset-0 flex items-center justify-center">
         <h1 className='text-[220px] z-10 relative'>HESTIA</h1>
       </div>
