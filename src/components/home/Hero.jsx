b4f7157553d92c9dad63909d48fd2f9a5960408a
vagabond-0 <220229@tkmce.ac.in>
2025-03-01 21:56:56 +0530

    done

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -5,14 +5,14 @@ import Headerbackground2 from "../../assets/images/hero/background2.webp"
 
 const Hero = () => {
   return (
-    <div className='section relative' style={{ fontFamily: 'rubik' }}>
+    <div className='section relative min-h-screen' style={{ fontFamily: 'rubik' }}>
       <img src={Headerbackground} className='w-full h-80 absolute z-10' />
-      <div className="text-center absolute inset-0 flex items-center justify-center">
-        <h1 className='md:text-[220px] text-7xl sm:text-9xl z-20 absolute'>HESTIA</h1>
+      <div className="text-center  inset-0 flex items-center justify-center">
+        <h1 className='md:text-[220px] text-7xl sm:text-9xl z-20 relative'>HESTIA</h1>
       </div>
       <div className='halftone'></div>
       <img src={Headerbackground2} className='w-full h-80 absolute z-10 -bottom-16' />
-      <div className="absolute w-full bg-black h-[160px] -bottom-16 rotate-[3deg] "></div>
+      <div className="absolute w-full bg-black h-[158px] -bottom-16 rotate-3 "></div>
     </div>
   )
 }
