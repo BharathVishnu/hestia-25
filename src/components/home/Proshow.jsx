329543538d5f3ca34eeb6c5812d60f64da4e5e70
Bharath Vishnu B <bharathvb1204@gmail.com>
2025-03-02 17:05:32 +0530

    navbar route sponsors2

--- src/components/home/Proshow.jsx
+++ src/components/home/Proshow.jsx
@@ -1,88 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import bg from "../../assets/images/bg_proshow.png"
-import pro1 from '../../assets/images/pro1.png'
-import prb1 from '../../assets/images/imageyellow.png'
-import pro2 from '../../assets/images/pro2.png'
-import pro3 from '../../assets/images/pro3.png'
-import '../../styles/proshow.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import bg from "../../assets/images/bg_proshow.png"
+// import pro1 from '../../assets/images/pro1.png'
+// import prb1 from '../../assets/images/imageyellow.png'
+// import pro2 from '../../assets/images/pro2.png'
+// import pro3 from '../../assets/images/pro3.png'
+// import '../../styles/proshow.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-
-        return (
-    <div style={{ backgroundImage: `url(${bg})`, height: "auto", 
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat"}}>
-
-        <div className="container mx-auto px-4 py-8">
-        <h1 className="text-center text-4xl font-bold mb-8">PROSHOWS</h1>
+// function App() {
+//   const [count, setCount] = useState(0)
 
 
-    <div className="space-y-8">
-{/*Event1*/}
-        <div className="flex flex-col md: flex-row ">
+//         return (
+//     <div style={{ backgroundImage: `url(${bg})`, height: "auto", 
+//     backgroundSize: "cover",
+//     backgroundRepeat: "no-repeat"}}>
+
+//         <div className="container mx-auto px-4 py-8">
+//         <h1 className="text-center text-4xl font-bold mb-8">PROSHOWS</h1>
+
+
+//     <div className="space-y-8">
+// {/*Event1*/}
+//         <div className="flex flex-col md: flex-row ">
             
-            <div className='relative mb-4 md:mb-0'>
-                <img
-                    src={pro1}
-                    className="w-[400px] h-[200px] 
-                    flex-shrink-0 aspect-[840.24/606.01] max-w-full h-auto" />
-                <img
-                    src={prb1}
-                    className="w-[400px] h-[200px] 
-                    flex-shrink-0 aspect-[840.24/606.01] max-w-full h-auto absolute bottom-2 right-2" />
+//             <div className='relative mb-4 md:mb-0'>
+//                 <img
+//                     src={pro1}
+//                     className="w-[400px] h-[200px] 
+//                     flex-shrink-0 aspect-[840.24/606.01] max-w-full h-auto" />
+//                 <img
+//                     src={prb1}
+//                     className="w-[400px] h-[200px] 
+//                     flex-shrink-0 aspect-[840.24/606.01] max-w-full h-auto absolute bottom-2 right-2" />
                 
-                <div className="absolute bottom-4 md:bottom-auto md:top-[60px] md:right-[-185px] text-right right-4"> 
-                <div><h2 className="text-lg font-bold">DJ STRELLA</h2></div>
-                <div className='md:absolute md:right-[-50px]'><h3 className="text-sm ">04-04-2023</h3></div>
-                </div>
-            </div>  
-        </div>
+//                 <div className="absolute bottom-4 md:bottom-auto md:top-[60px] md:right-[-185px] text-right right-4"> 
+//                 <div><h2 className="text-lg font-bold">DJ STRELLA</h2></div>
+//                 <div className='md:absolute md:right-[-50px]'><h3 className="text-sm ">04-04-2023</h3></div>
+//                 </div>
+//             </div>  
+//         </div>
         
-{/*Event2*/}
-        <div className="flex flex-col md:flex-row-reverse">
-            <div className='relative mb-4 md:mb-0'>
+// {/*Event2*/}
+//         <div className="flex flex-col md:flex-row-reverse">
+//             <div className='relative mb-4 md:mb-0'>
             
-                <img
-                    src={pro2} 
-                    className="w-[400px] h-[200px] flex-shrink-0 aspect-[840.24/606.01] max-w-full h-auto" />
-                    <img
-                    src={prb1}
-                    className="w-[400px] h-[200px] 
-                    flex-shrink-0 aspect-[840.24/606.01] max-w-full h-auto absolute bottom-2 right-2" />
-                    <div className="absolute bottom-4 right-4 text-right md:right-99 md:top-20 md:text-left whitespace-nowrap">
-                    <div><h2 className="text-lg font-bold " >DJ STRELLA</h2></div>
-                    <div className='md:absolute md:left-[-60px] '><h3 className="text-sm">03-04-2023</h3></div>
-                </div>
+//                 <img
+//                     src={pro2} 
+//                     className="w-[400px] h-[200px] flex-shrink-0 aspect-[840.24/606.01] max-w-full h-auto" />
+//                     <img
+//                     src={prb1}
+//                     className="w-[400px] h-[200px] 
+//                     flex-shrink-0 aspect-[840.24/606.01] max-w-full h-auto absolute bottom-2 right-2" />
+//                     <div className="absolute bottom-4 right-4 text-right md:right-99 md:top-20 md:text-left whitespace-nowrap">
+//                     <div><h2 className="text-lg font-bold " >DJ STRELLA</h2></div>
+//                     <div className='md:absolute md:left-[-60px] '><h3 className="text-sm">03-04-2023</h3></div>
+//                 </div>
         
-            </div>
-        </div>
+//             </div>
+//         </div>
 
-{/*Event3*/}
+// {/*Event3*/}
         
-        <div className="flex flex-col md: flex-row">
-            <div className='relative mb-4 md:mb-0'>
-                <img
-                    src={pro3}
-                    className="w-[400px] h-[200px] flex-shrink-0 aspect-[840.24/606.01] max-w-full h-auto" />
-                    <img
-                    src={prb1}
-                    className="w-[400px] h-[200px] 
-                    flex-shrink-0 aspect-[840.24/606.01] max-w-full h-auto absolute bottom-2 right-2" />
-                    <div className="absolute bottom-4 md:bottom-auto md:top-[60px] md:right-[-185px] text-right right-4"> 
-                    <div><h2 className="text-lg font-bold">DJ STRELLA</h2></div>
-                    <div className='md:absolute md:right-[-50px]'><h3 className="text-sm ">04-04-2023</h3></div>
-                </div>
-            </div>
-        </div>
-        </div>
-    </div>
-  </div>
+//         <div className="flex flex-col md: flex-row">
+//             <div className='relative mb-4 md:mb-0'>
+//                 <img
+//                     src={pro3}
+//                     className="w-[400px] h-[200px] flex-shrink-0 aspect-[840.24/606.01] max-w-full h-auto" />
+//                     <img
+//                     src={prb1}
+//                     className="w-[400px] h-[200px] 
+//                     flex-shrink-0 aspect-[840.24/606.01] max-w-full h-auto absolute bottom-2 right-2" />
+//                     <div className="absolute bottom-4 md:bottom-auto md:top-[60px] md:right-[-185px] text-right right-4"> 
+//                     <div><h2 className="text-lg font-bold">DJ STRELLA</h2></div>
+//                     <div className='md:absolute md:right-[-50px]'><h3 className="text-sm ">04-04-2023</h3></div>
+//                 </div>
+//             </div>
+//         </div>
+//         </div>
+//     </div>
+//   </div>
 
-    )
+//     )
 
-}    
-export default App
+// }    
+// export default App
