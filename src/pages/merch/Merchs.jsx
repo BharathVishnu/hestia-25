38b4624e4672bbeb0ea073a29d1c2c77d78cacb5
Aswinjp-max <aswinjp2255@gmail.com>
2025-03-03 10:05:07 +0530

    commit7

--- src/pages/merch/Merchs.jsx
+++ src/pages/merch/Merchs.jsx
@@ -14,15 +14,15 @@ const Merchs = () => {
       
       <div className="flex flex-col items-center bg-[url(/src/assets/images/faqbg.png)] py-24">
         <div className="relative w-screen h-4 md:h-12">
-          <div className="bg-[#720A08] absolute right-1 top-0 w-24 md:w-48 h-1/4 rounded-2xl"></div>
-          <div className="bg-white absolute right-16 bottom-0 w-24 md:w-48 h-1/4 rounded-2xl"></div>
+          <div className="bg-[#720A08] absolute right-1 top-0 w-24 md:w-48 h-1/4 "></div>
+          <div className="bg-white absolute right-16 bottom-0 w-24 md:w-48 h-1/4 "></div>
         </div>
         
         <h1 className="text-4xl sm:text-6xl md:text-8xl lg:text-9xl tracking-widest font-bold text-center uppercase font-poppins">Available Now</h1>
         
         <div className="relative w-screen h-4 md:h-12">
-          <div className="bg-[#720A08] absolute left-16 top-0 w-24 md:w-48 h-1/4 rounded-2xl"></div>
-          <div className="bg-white absolute left-1 bottom-0 w-24 md:w-48 h-1/4 rounded-2xl"></div>
+          <div className="bg-[#720A08] absolute left-16 top-0 w-24 md:w-48 h-1/4 "></div>
+          <div className="bg-white absolute left-1 bottom-0 w-24 md:w-48 h-1/4 "></div>
         </div>
         
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-20 mt-10  w-full pl-5 pr-5 ">
@@ -43,7 +43,7 @@ const Merchs = () => {
             <div className="bg-white p-4 text-center text-black w-full h-full">
               <img src={shirt2} alt="Product" className="w-full h-auto" />
             </div>
-            <div className="bg-black p-8 w-full border-2 border-white mt-[-20px]">
+            <div className="bg-black p-8 w-full border-2 border-white hover:border-[#720A08] mt-[-20px]">
               <h2 className="2xl:text-5xl xl:text-4xl lg:text-3xl  leading-[1.2] font-normal uppercase text-white text-center" style={{ fontFamily: 'Rubikvinyl' }}>NAME</h2>
             </div>
           </div>
@@ -53,11 +53,18 @@ const Merchs = () => {
             <div className="bg-white p-4 text-center text-black w-full h-full">
               <img src={shirt3} alt="Product" className="w-full h-auto" />
             </div>
-            <div className="bg-black p-8 w-full border-2 border-[#720A08] mt-[-20px]">
+            <div className="bg-black p-8 w-full border-2 border-white hover:border-[#720A08] mt-[-20px]">
               <h2 className="2xl:text-5xl xl:text-4xl lg:text-3xl  leading-[1.2] font-normal uppercase text-white text-center" style={{ fontFamily: 'Rubikvinyl' }}>NAME</h2>
             </div>
           </div>
+          
         </div>
+        
+      </div>
+      <div className="flex flex-row items-center mx-auto justify-center  w-auto lg:w-[800px] xl:w-[1200px] h-1 md:h-4 bg-[#720A08]"></div>
+      <div className="flex flex-col gap-8 relative w-screen h-10 md:h-14">
+        <div className="bg-white absolute  left-0 top-1 w-24 md:w-64 h-2/4 "></div>
+        <div className="bg-white absolute right-1 bottom-0 w-24 md:w-64 h-1/4 "></div>
       </div>
     </div>
   );
